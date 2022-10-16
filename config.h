1ec04d88faa54f8e919c9ca4fc4a069791709300
Tortus-exe <65506144+Tortus-exe@users.noreply.github.com>
2022-10-16 02:06:29 -0700

    set some settings

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xe0;
+static const unsigned int alpha = 0xf0;
 static int centered = 0;										/* -c option; centers dmenu on screen */
 static int min_width = 500;									/* minimum width when centered */
 static unsigned int border_width = 0;
@@ -13,10 +13,10 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { "#cdd6f4", "#1E1E2E" },
+	[SchemeSel] = { "#eeeeee", "#730e0e" },
+	[SchemeSelHighlight] = { "#ffc978", "#73000e" },
+	[SchemeNormHighlight] = { "#ffc978", "#1E1E2E" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid of > 0 */
